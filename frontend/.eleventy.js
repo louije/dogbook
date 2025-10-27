94f5432b592637302f122236853fec9c70d09141
Louis-Jean Teitelbaum <ljt@meidosem.com>
2025-10-27 12:46:40 +0100

    Fix GraphQL query - replace age with birthday field

--- frontend/.eleventy.js
+++ frontend/.eleventy.js
@@ -58,7 +58,7 @@ module.exports = function(eleventyConfig) {
                 id
                 name
                 sex
-                age
+                birthday
                 breed
                 coat
                 description {
