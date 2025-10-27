a171c5045c9c61695afdfe08d8a11c21650057ef
Louis-Jean Teitelbaum <ljt@meidosem.com>
2025-10-27 12:31:17 +0100

    Fix JSX import - use React instead of @keystone-ui/core

--- backend/admin/config.tsx
+++ backend/admin/config.tsx
@@ -1,4 +1,4 @@
-/** @jsxImportSource @keystone-ui/core */
+import React from 'react';
 import { NavigationContainer, NavItem, ListNavItems } from '@keystone-6/core/admin-ui/components';
 import type { NavigationProps } from '@keystone-6/core/admin-ui/components';
 
