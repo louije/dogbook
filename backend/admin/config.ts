4b83bd71466a6f9e222f7310673ef6ed5f1f5dea
Louis-Jean Teitelbaum <user@example.com>
2025-10-27 14:52:51 +0100

    Properly separate admin config.ts and CustomNavigation component

--- backend/admin/config.ts
+++ backend/admin/config.ts
@@ -1,22 +1,6 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@keystone-ui/core';
-import type { NavigationProps } from '@keystone-6/core/admin-ui/components';
-import { NavigationContainer, NavItem, ListNavItems } from '@keystone-6/core/admin-ui/components';
 import type { AdminConfig } from '@keystone-6/core/types';
-
-function CustomNavigation({ lists, authenticatedItem }: NavigationProps) {
-  return (
-    <NavigationContainer>
-      <NavItem href="/">Dashboard</NavItem>
-      <ListNavItems lists={lists} />
-      {!authenticatedItem && (
-        <NavItem href="/signin">Se connecter</NavItem>
-      )}
-    </NavigationContainer>
-  );
-}
+import { CustomNavigation } from './components/CustomNavigation';
 
 export const components: AdminConfig['components'] = {
-  Navigation: CustomNavigation,
+  Navigation: CustomNavigation
 };
